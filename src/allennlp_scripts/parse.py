b66e40f72c024a8233e637b32336cb0c8a8e0ed9
eahogue <eahogue@gmail.com>
2021-03-09 11:49:08 -0800

    Settings vars added and cleaned up, pythonified.

--- src/allennlp_scripts/parse.py
+++ src/allennlp_scripts/parse.py
@@ -26,38 +26,42 @@ import os
 
 
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+CWD = os.getcwd()
 
+#settings.sh
 # Folders
-NEURAL_GIT=$(pwd)
-DRS_GIT="${NEURAL_GIT}/DRS_parsing/"
-SRC_PYTHON="${NEURAL_GIT}/src/"
+NEURAL_GIT = CWD
+DRS_GIT = NEURAL_GIT + "/DRS_parsing"
+SRC_PYTHON = NEURAL_GIT + "/src"
 
 # Files
-PP_PY="${SRC_PYTHON}postprocess.py"
-SIG_FILE="${DRS_GIT}evaluation/clf_signature.yaml"
-COUNTER="${DRS_GIT}evaluation/counter.py"
+PP_PY = SRC_PYTHON + "/postprocess.py"
+SIG_FILE = DRS_GIT + "/evaluation/clf_signature.yaml"
+COUNTER = DRS_GIT + "/evaluation/counter.py"
 
 # Setting for postprocessing
-REMOVE_CLAUSES=75
-SEP="|||"
+REMOVE_CLAUSES = 75
+SEP = "|||"
 # Use this for word-based exps
-no_sep="--no_sep"
-MIN_TOKENS=10
+no_sep = "--no_sep"
+MIN_TOKENS = 10
 
 # NOTE:
 # Use this for character-level exps
-#no_sep=""
-#MIN_TOKENS=20
+#no_sep = ""
+#MIN_TOKENS = 20
 
 # Settings for experiments
-FORCE_PP=true       # whether we force postprocessing or skip if already there
-SILENT="--silent"   # silent parsing
-FORCE="-f"          # force reprocessing if directory already exists
+FORCE_PP = True       # whether we force postprocessing or skip if already there
+SILENT = "--silent"   # silent parsing
+FORCE = "-f"          # force reprocessing if directory already exists
 
 # For tarring our own models
-VOCAB="vocabulary/"
-CONFIG="config.json"
-model_file="model.tar.gz"
+VOCAB = "vocabulary/"
+CONFIG = "config.json"
+model_file = "model.tar.gz"
+#/settings.sh
+
 
 def main():
     """ """
