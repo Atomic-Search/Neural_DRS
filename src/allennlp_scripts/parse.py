7dfeab16f0181b4df2c3c7d6fbf96ed6936f0204
eahogue <eahogue@gmail.com>
2021-03-09 16:13:05 -0800

    Finished fucntion, cleaned up a bit.

--- src/allennlp_scripts/parse.py
+++ src/allennlp_scripts/parse.py
@@ -8,8 +8,10 @@ Discourse Representation Theory model for parsing. It replaces "parse.sh".
 For now we are working at the sentence level, but this will probably change
 later. 
 
+Note: this script does not work when using multiple encoders.
+
 Input: Raw text sentence (or potentially more, up to 50 characters for
-    now.
+    now).
 
 Output: A discourse representation structure that analyzes the meaning
     of the input text.
@@ -25,8 +27,6 @@ __version__ = "0.1.0"
 
 
 import os
-import postprocess as pp
-
 
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 CWD = os.getcwd()
@@ -46,12 +46,12 @@ COUNTER = DRS_GIT + "/evaluation/counter.py"
 REMOVE_CLAUSES = 75
 SEP = "|||"
 # Use this for word-based exps
-no_sep = "--no_sep"
+NO_SEP = "--no_sep"
 MIN_TOKENS = 10
 
 # NOTE:
 # Use this for character-level exps
-#no_sep = ""
+#NO_SEP = ""
 #MIN_TOKENS = 20
 
 # Settings for experiments
@@ -62,70 +62,55 @@ FORCE = "-f"      # force reprocessing if directory already exists
 # For tarring our own models  ??
 VOCAB = "vocabulary/"
 CONFIG = "config.json"
-model_file = "model.tar.gz"
+MODEL_FILE = "model.tar.gz"
 #/settings.sh
-
-#parse.sh
-# Parse raw text with an AllenNLP model
-# Note: this script does not work when using multiple encoders
-# Arguments: $1: input raw file (not tokenized!)
-#        $2: model file
-#        $3: target vocab
 
 CURRENT_MODEL = f"{CWD}/models/allennlp/bert_char_1enc.tar.gz"
 VOCAB_FILE = f"{CWD}/vocabs/allennlp/tgt_bert_char_1enc.txt"
+INPUT_FILE = "drs_parse_input.drs"
+OUTPUT_FILE = "drs_parse_output.txt"
 
 os.environ["PYTHONPATH"] = DRS_GIT + "/evaluation/:${PYTHONPATH}"
 os.environ["PYTHONPATH"] = NEURAL_GIT + "/src/:${PYTHONPATH}"
 
 def parse(text, 
-      model=CURRENT_MODEL, 
-      vocab_file=VOCAB_FILE):
-    """[summary]
+        model=CURRENT_MODEL, 
+        vocab_file=VOCAB_FILE,
+        for_humans=False,
+        ):
+    """
+    This function is an interface around Neural DSR's prediction
+    commands and assorted settings. It is intended to be imported
+    into the Atomic pipeline, processing one sentence (or unit
+    of text) at a time and returning the machine-readable discourse
+    representation structure.
 
     Args:
-        text ([type]): [description]
-        model ([type], optional): [description]. Defaults to CURRENT_MODEL.
-        vocab_file ([type], optional): [description]. Defaults to VOCAB_FILE.
+        text        (string):           A plain string of one or more sentences.
+        model       (str, optional):    Location of the model to use. Defaults to CURRENT_MODEL.
+        vocab_file  (str, optional):    Location of the vocab to use. Defaults to VOCAB_FILE.
+        for_humans  (bool, optional):   True if you want DRS formatted for human consumption.
     """
     # Put raw text in format we read with our dataset reader, i.e. add dummy DRS after each line
     text = text.strip()
     text = text + "\tDummy"
 
-#alp_file=${raw_file}.alp
-#cp $raw_file $alp_file
-#TAB=$'\t'
-#sed -e "s/$/${TAB}Dummy/" -i $alp_file
+    # It's not very pretty, but for now we have to turn this input into a file. 
+    # TODO: Figure out how to avoid these otherwise unnecessary files.
+    with open(INPUT_FILE, "w") as inputf:
+        print(text, file=inputf)
 
     # Do the predicting -- to start we'll use the shell command. Pythonize this later.
+    os.system(f"allennlp predict {CURRENT_MODEL} {INPUT_FILE} --use-dataset-reader --cuda-device 0 --predictor seq2seq --output-file {OUTPUT_FILE} {SILENT}")
+
+    # Now do postprocessing, replace ill-formed DRSs by dummies
+    os.system(f"python {PP_PY} --input_file {OUTPUT_FILE} --output_file {OUTPUT_FILE}.out --sig_file {SIG_FILE} --fix --json --sep {SEP} -rcl {REMOVE_CLAUSES} -m {MIN_TOKENS} -voc {VOCAB_FILE} {NO_SEP}")
+
+    # Read back in the resulting DRS.
+    with open(f"{OUTPUT_FILE}.out", "r") as outputf:
+        drs_parse = outputf.read()
+
+    # Remove temporary files (clean up)
+    os.system(f"rm {OUTPUT_FILE}; rm {INPUT_FILE}; rm {OUTPUT_FILE}.out")
     
-    os.system("allennlp predict model text --use-dataset-reader --cuda-device 0 --predictor seq2seq $SILENT")
-
-#out_file=${raw_file}.drs
-#allennlp predict $cur_model $alp_file --use-dataset-reader --cuda-device 0 --predictor seq2seq --output-file $out_file $SILENT
-
-    output = do_postprocess(args)
-
-# Now do postprocessing, replace ill-formed DRSs by dummies
-python $PP_PY --input_file $out_file 
-        --output_file ${out_file}.out  
-        --sig_file $SIG_FILE 
-        --fix 
-        --json 
-        --sep $SEP 
-        -rcl $REMOVE_CLAUSES 
-        -m $MIN_TOKENS 
-        -voc $vocab_file 
-        $no_sep
-
-# Remove temporary .alp file (clean up)
-rm $alp_file
-
-
-def main():
-    """ """
-
-
-if __name__ == "__main__":
-    """  """
-    main()
+    return drs_parse
